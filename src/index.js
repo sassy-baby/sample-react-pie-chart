94366ec60e777df680f10455804b4d9ad2e4a30b
sassy <19832133+sassy-baby@users.noreply.github.com>
2019-07-24 19:13:23 +0900

    create PieChart

--- src/index.js
+++ src/index.js
@@ -1,21 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-import { RadialChart } from "react-vis";
-
+import { VictoryPie, VictoryAnimation, VictoryLabel } from "victory";
 import "./styles.css";
-const myData = [{ angle: 80 }, { angle: 20 }];
 
 function App() {
+  const [percent, setPercent] = useState(20);
+  // const [data, setData] = useState([{ x: 1, y: 0 }, { x: 2, y: 100 - 0 }]);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      percent === 100 && clearInterval(interval);
+      percent < 100 && setPercent(percent + 1);
+    }, 300);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [percent]);
+
   return (
     <div className="App">
-      <RadialChart
-        data={myData}
-        animation={{ damping: 9, stiffness: 300 }}
-        width={300}
-        height={300}
-        radius={100}
-        innerRadius={80}
-      />
+      <svg viewBox="0 0 400 400" width="100%" height="100%">
+        <VictoryPie
+          standalone={false}
+          animate={{ duration: 1000 }}
+          width={400}
+          height={400}
+          data={[{ x: 1, y: percent }, { x: 2, y: 100 - percent }]}
+          innerRadius={120}
+          cornerRadius={25}
+          labels={() => null}
+          style={{
+            data: {
+              fill: d => {
+                const color = d.y > 30 ? "green" : "red";
+                return d.x === 1 ? color : "transparent";
+              }
+            }
+          }}
+        />
+        <VictoryAnimation
+          duration={1000}
+          data={{
+            percent,
+            data: [{ x: 1, y: percent }, { x: 2, y: 100 - percent }]
+          }}
+        >
+          {newProps => {
+            return (
+              <VictoryLabel
+                textAnchor="middle"
+                verticalAnchor="middle"
+                x={200}
+                y={200}
+                text={`${Math.round(percent)}%`}
+                style={{ fontSize: 45 }}
+              />
+            );
+          }}
+        </VictoryAnimation>
+      </svg>
     </div>
   );
 }
